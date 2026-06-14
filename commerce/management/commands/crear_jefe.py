b1643bb2a679ef879cde9656b03ff70bd521a673
JuanAndres PintoMeza <lafamiliapinto123@gmail.com>
2026-06-13 22:41:00 -0500

    fix: corregir campos del modelo User en crear_jefe

--- commerce/management/commands/crear_jefe.py
+++ commerce/management/commands/crear_jefe.py
@@ -5,13 +5,15 @@ class Command(BaseCommand):
     help = 'Crea el usuario jefe inicial'
 
     def handle(self, *args, **kwargs):
-        if User.objects.filter(identificacion='1234567890').exists():
+        if User.objects.filter(identification='1234567890').exists():
             self.stdout.write('El usuario ya existe, omitiendo.')
             return
         u = User()
-        u.nombre = 'Juan Andres Pinto'
-        u.identificacion = '1234567890'
-        u.rol = 'jefe'
+        u.full_name = 'Juan Andres Pinto'
+        u.identification = '1234567890'
+        u.username = '1234567890'
+        u.role = 'jefe'
+        u.is_staff = True
         u.set_password('admin123')
         u.save()
         self.stdout.write('Usuario jefe creado exitosamente.')
